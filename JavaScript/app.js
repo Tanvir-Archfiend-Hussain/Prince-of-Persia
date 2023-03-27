404a21c1b69d544de7d8ba6f34376c9dbed742fb
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-03-27 21:43:25 +0100

    JS

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -2,7 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const prince = document.querySelector(".character");
   let bottom = 0;
 
-  function jump() {}
+  function jump() {
+    bottom += 30;
+    prince.style.bottom = bottom + "px";
+    //adding 30
+  }
 
   jump();
+
+  //assign functions to keycode
 });
