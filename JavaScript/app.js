df94f10ccdbf8b397d019f108761d06f093b6250
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-03-27 21:44:24 +0100

    JS

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -5,10 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
   function jump() {
     bottom += 30;
     prince.style.bottom = bottom + "px";
-    //adding 30
+    //adding 30 px to the bottom to send the prince up
   }
 
   jump();
 
   //assign functions to keycode
+  function control(e) {
+    if (e.jeyCode)
+  }
 });
