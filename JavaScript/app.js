501a492c05d8a69f3acb1190f62fafc1212c97e7
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-03-28 21:54:35 +0100

    JS

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const prince = document.querySelector(".character");
-  let bottom = 0;
+  let bottom = 0; /* Making a variable called 'bottom' and then giving it a value of 0  */
 
+  /** Creating function called Jump */
   function jump() {
-    bottom += 30;
+    bottom += 30; /* Making the bottom variable go from  being 0 to being 30*/
     prince.style.bottom = bottom + "px";
-    //adding 30 px to the bottom to send the prince up
+    /* Adding 30 px to the bottom to send the prince up */
   }
 
-  jump();
+  jump(); /** Invoking the function */
 
-  //assign functions to keycode
+  /** Assign functions to keycode */
   function control(e) {
     if (e.keyCode === 38) {
       jump();
