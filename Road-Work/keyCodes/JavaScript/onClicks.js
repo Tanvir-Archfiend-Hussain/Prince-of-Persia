ebbb847a1247aec122f387e29f10e554c287a184
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-03-30 21:59:02 +0100

    Update

--- Road-Work/keyCodes/JavaScript/onClicks.js
+++ Road-Work/keyCodes/JavaScript/onClicks.js
@@ -1,10 +1,4 @@
 /** Pressing key to open window */
-function OpenWin() {
+function OpenWin586985() {
   window.open("https://static.toiimg.com/photo/msid-71761024/71761024.jpg");
 }
-function control02(e) {
-  if (e.keyCode === 40) {
-    OpenWin();
-  }
-}
-document.addEventListener("keydown", control02);
